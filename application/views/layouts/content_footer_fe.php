40870f2561be70fe65585ccb029ee9bba2b2c4d0
chandraahmad <candraaklapikas@gmail.com>
2021-11-21 20:10:33 +0700

    change lable

--- application/views/layouts/content_footer_fe.php
+++ application/views/layouts/content_footer_fe.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        El-Rusta Studio
+        Pos Indonesia
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2021 Pos Indonesia</strong> All rights reserved.
